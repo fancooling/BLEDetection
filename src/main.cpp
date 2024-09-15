a31cad7ae78793874fdda75e1c68805586132bfb
fanjieyan <fanjieyan@gmail.com>
2024-09-15 12:57:16 -0700

    test:

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,14 @@ const int CUTOFF = -70;
 // const std::string DEVICE_TO_FIND("d0:3f:27:60:3b:30");  // Wyze Scale
 // const std::string DEVICE_TO_FIND("ec:a7:cd:8b:af:8c");  // Air tag
 // const std::string DEVICE_TO_FIND("51:96:d5:d8:3b:d4");  // fanbl S23 ultra advertiser
-const std::string UUID_TO_FIND("bc157f34-328e-412e-a45d-8180ff9b401a");  // fanbl S23 ultra advertiser
+static BLEUUID UUIDS_TO_FIND[] = {
+  BLEUUID("bc157f34-328e-412e-a45d-8180ff9b401a"),  // fanbl S23 ultra advertiser simulator
+  BLEUUID("0000fffd-0000-1000-8000-00805f9b34fb")  // BLE login key
+};
+
+static std::string MANUFACTURER_DATA[] = {
+  "4c00121990fb76a061d0b35048f1b1bada2b37a15badd29ef06823037c"  // air tag
+};
 
 void setup() {
   Serial.begin(9600);  
@@ -24,36 +31,37 @@ void setup() {
   ESP_LOGI(TAG, "start testing");
 }
 
+bool isDeviceDetected(BLEAdvertisedDevice& device) {
+  if (device.getRSSI() > CUTOFF) {
+    ESP_LOGI(TAG, "Nearby device found: %s\n", device.toString().c_str());
+    //  Serial.printf("In loop device found %s\n", device.toString().c_str());
+    for (BLEUUID& uuid : UUIDS_TO_FIND) {
+      if (device.isAdvertisingService(uuid)) {
+        ESP_LOGW(TAG, "Detected Device by UUID: %s\n", device.toString().c_str());
+        return true;
+      }
+    }
+    for (const std::string& manufactorer_data : MANUFACTURER_DATA) {
+      if (device.getManufacturerData() == manufactorer_data) {
+        ESP_LOGW(TAG, "Detected Device by manufactorer data: %s\n", device.toString().c_str());
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 void loop() {
-  BLEUUID uuid_to_find(UUID_TO_FIND);
   BLEScan *scan = BLEDevice::getScan();
   scan->setActiveScan(true);
   BLEScanResults results = scan->start(1);
   bool found = false;
   for (int i = 0; i < results.getCount(); i++) {
     BLEAdvertisedDevice device = results.getDevice(i);
-    int rssi = device.getRSSI();
-    if (rssi > CUTOFF) {
-      // ESP_LOGI(TAG, "Device found: %s\n", device.toString().c_str());
-      Serial.printf("In loop device found %s\n", device.toString().c_str());       
-      if (device.isAdvertisingService(uuid_to_find)) {
-        ESP_LOGW(TAG, "Detected Device: %s\n", device.toString().c_str());
-        found = true;
-      }
-    }    
-    
-      /*if (rssi > CUTOFF) {
-        found = true;
-      }*/
-    //}    
-    /*
-    if (rssi > best) {
-      best = rssi;
-      ESP_LOGI(TAG, "ESPlog Device found: %ls, address: %s\n", device.getName().c_str(), device.getAddress().toString().c_str());
-      ESP_LOGI(TAG, "ESPlog Device: %s\n", device.toString().c_str());
+    if (isDeviceDetected(device)) {
+      found = true;
+      break;
     }
-    */
-    //delay(1000);
   }
   // digitalWrite(PIN, best > CUTOFF ? HIGH : LOW);
   digitalWrite(PIN, found ? HIGH : LOW);
