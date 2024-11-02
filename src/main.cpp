5c78f23d1b152ce713851f12255a5babc864a711
fanjieyan <fanjieyan@gmail.com>
2024-11-02 09:54:11 -0700

    edit

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,16 @@
 
 static const char* TAG = "MyScanModule";
 const int PIN = 2;
-const int CUTOFF = -70;
+const int CUTOFF = -65;
 // const std::string DEVICE_TO_FIND("d0:3f:27:60:3b:30");  // Wyze Scale
 // const std::string DEVICE_TO_FIND("ec:a7:cd:8b:af:8c");  // Air tag
 // const std::string DEVICE_TO_FIND("51:96:d5:d8:3b:d4");  // fanbl S23 ultra advertiser
 static BLEUUID UUIDS_TO_FIND[] = {
   BLEUUID("bc157f34-328e-412e-a45d-8180ff9b401a"),  // fanbl S23 ultra advertiser simulator
   BLEUUID("0000fffd-0000-1000-8000-00805f9b34fb"),  // BLE login key
-  BLEUUID("0000ffe0-0000-1000-8000-00805f9b34fb"),  // iTag Blue
-  BLEUUID("0000ffe0-0000-1000-8000-00805f9b34fb")  // iTag Pink
-  // BLEUUID("0000feed-0000-1000-8000-00805f9b34fb"),  // Tile 1
-  // BLEUUID("0000feed-0000-1000-8000-00805f9b34fb")   // Tile 2
+  BLEUUID("0000ffe0-0000-1000-8000-00805f9b34fb")  // iTag
+//  BLEUUID("0000feed-0000-1000-8000-00805f9b34fb")  // Tile 1
+//  BLEUUID("0000feed-0000-1000-8000-00805f9b34fb")   // Tile 2
 };
 
 void setup() {
@@ -34,7 +33,7 @@ void setup() {
 bool isDeviceDetected(BLEAdvertisedDevice& device) {
   // ESP_LOGI(TAG, "Signal found: %s\n", device.toString().c_str());
   if (device.getRSSI() > CUTOFF) {
-    ESP_LOGD(TAG, "Nearby device found: %s\n", device.toString().c_str());
+    ESP_LOGI(TAG, "Nearby device found: %s\n", device.toString().c_str());
     for (BLEUUID& uuid : UUIDS_TO_FIND) {
       if (device.isAdvertisingService(uuid)) {
         ESP_LOGI(TAG, "Detected Device by UUID: %s\n", device.toString().c_str());        
@@ -58,6 +57,10 @@ void loop() {
     }
   }
 
-  digitalWrite(PIN, found ? HIGH : LOW);
-  delay(1000);  // wait 1s each loop
+  if (found) {
+    digitalWrite(PIN, HIGH);
+    delay(2000);  // beep for 2s
+  }
+  digitalWrite(PIN, LOW);  
+  // delay(1000);  // wait 1s each loop
 }
